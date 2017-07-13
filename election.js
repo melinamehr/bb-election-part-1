883b6a7211609dee5f4be20a398079e5598b77ef
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-07-13 15:57:22 -0400

    created a working loop

--- election.js
+++ election.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(responseData) {
+    for (var i = 0; i < responseData.candidates.length; i++) {
+      var candidate = responseData.candidates[i]
+      console.log(candidate)
+    }
+
+  })
 
 });
